e8100b4b2a988850e22468bf391df03838596ad8
1jeidh <hungqh7@gmail.com>
2025-03-12 18:15:06 +0700

    Finish Moving Enemies

--- Platform_Demo/Scripts/enemy_slime.gd
+++ Platform_Demo/Scripts/enemy_slime.gd
@@ -1,6 +1,12 @@
 extends Node2D
 
 @onready var timer: Timer = $Area2D/Timer
+@onready var ray_cast_2d_left: RayCast2D = $RayCast2D_Left
+@onready var ray_cast_2d_right: RayCast2D = $RayCast2D_Right
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+
+const SPEED = 300.0
+var direction = 1
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -8,7 +14,13 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if ray_cast_2d_right.is_colliding():
+		direction = -1
+		animated_sprite_2d.flip_h = false
+	if ray_cast_2d_left.is_colliding():
+		direction = 1
+		animated_sprite_2d.flip_h = true
+	position.x += direction * SPEED * delta
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if(body.name == "CharacterBody2D"):
